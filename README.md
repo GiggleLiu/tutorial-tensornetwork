992829fd44f658f3e79416f7ef9f0a37acc4e20c
GiggleLiu <cacate0129@gmail.com>
2025-07-18 09:58:19 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tensor networks for quantum circuit simulation and quantum error correction
+
+Table of contents:
+
+- [Introduction to tensor networks](codes/basic.jl)
+- [Quantum circuit simulation](codes/circuitsim.jl)
+- [Quantum error correction](codes/qec.jl)
--- codes/qec.jl
+++ codes/qec.jl
@@ -1,7 +1,24 @@
 ## circuit level decode: surface code as an example
-using TensorQEC
+using TensorQEC, OMEinsum
 
 ## Exact contraction
+
+d = 9
+tanner = CSSTannerGraph(SurfaceCode(d, d))
+
+ct = compile(TNMAP(optimizer=TreeSA()), tanner)
+
+em = iid_error(0.05, 0.05, 0.05, d*d)
+ep = random_error_qubits(em)
+syn = syndrome_extraction(ep, tanner)
+
+# TODO: visualize syndrome
+
+res = decode(ct, syn)
+
+# TODO: visualize decoding result
+
+syndrome_extraction(res.error_qubits, tanner) == syn
 
 ## Belief propagation
 
