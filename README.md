3ff1127c61dc9b2eaddbafd77b9c146f38aa2c4d
GiggleLiu <cacate0129@gmail.com>
2025-08-02 17:09:19 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Tensor networks for quantum circuit simulation and quantum error correction
 
-Table of contents:
-
-- [Introduction to tensor networks](codes/basic.jl)
-- [Quantum circuit simulation](codes/circuitsim.jl)
-- [Quantum error correction](codes/qec.jl)
+Please check the note: [tnet.typ](tnet.typ) for the content.
--- examples/circuit_simulation/main.jl
+++ examples/circuit_simulation/main.jl
@@ -4,17 +4,16 @@ using Yao
 include("reader.jl")
 using .YaoQASMReader: yaocircuit_from_qasm
 
-using OMEinsum
+# load a visualize
+filename = joinpath(@__DIR__, "data", "sycamore_53_8_0.txt")
+@info "Circuit: $(filename)"
+c = yaocircuit_from_qasm(filename)
+vizcircuit(c)
 
-function main(; optimizer=TreeSA(), circuit_name = "sycamore_53_8_0")
-    @info("Running circuit `$(circuit_name)` with optimizer: $(optimizer)")
+# convert to tensor network and optimize the contraction order
+optimizer = TreeSA(ntrials=1)
+time_elapsed = @elapsed net = yao2einsum(c; initial_state=Dict(zip(1:nqubits(c), zeros(Int,nqubits(c)))), final_state=Dict(zip(1:nqubits(c), zeros(Int,nqubits(c)))), optimizer)
+@info "Contraction complexity: $(contraction_complexity(net)), time cost: $(time_elapsed)s"
 
-    # Create the TensorNetworkCircuit object for the circuit
-    file = joinpath(@__DIR__, "data", circuit_name * ".txt")
-    c = yaocircuit_from_qasm(file)
-    time_elapsed = @elapsed net = yao2einsum(c; initial_state=Dict(zip(1:nqubits(c), zeros(Int,nqubits(c)))), final_state=Dict(zip(1:nqubits(c), zeros(Int,nqubits(c)))), optimizer)
-    @info "Contraction complexity: $(contraction_complexity(net)), time cost: $(time_elapsed)s"
-    return contraction_complexity(net)
-end
-
-main()
+# contract!
+contract(net)
