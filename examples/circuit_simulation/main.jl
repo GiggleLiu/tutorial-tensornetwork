f27417ac8ef04d2a9b14d4ad7924c88760c53320
GiggleLiu <cacate0129@gmail.com>
2025-08-03 16:16:17 +0800

    update

--- examples/circuit_simulation/main.jl
+++ examples/circuit_simulation/main.jl
@@ -3,34 +3,59 @@ using Yao
 # circuit reader
 include("reader.jl")
 using .YaoQASMReader: yaocircuit_from_qasm
+using OMEinsum
 
-# load a visualize
-filename = joinpath(@__DIR__, "data", "circuits", "bristlecone_60_1-24-1_0.txt")
-@info "Circuit: $(filename)"
-c = yaocircuit_from_qasm(filename)
-vizcircuit(c)
+function dump_network(input::String; bitstring, output_folder, sc_target::Int=Inf, optimizer=TreeSA(ntrials=1), overwrite=false)
+    filename = joinpath(@__DIR__, "data", "circuits", input)
+    output = joinpath(@__DIR__, "data", "networks", output_folder)
+    isfile(output) && !overwrite && return
 
-# # convert to tensor network and optimize the contraction order
-# ## Case 1: compute <0|c|0>
-optimizer = TreeSA(ntrials=1)
-time_elapsed = @elapsed net = yao2einsum(c; initial_state=Dict(zip(1:nqubits(c), zeros(Int,nqubits(c)))), final_state=Dict(zip(1:nqubits(c), zeros(Int,nqubits(c)))), optimizer)
-@info "Contraction complexity: $(contraction_complexity(net)), time cost: $(time_elapsed)s"
-psi0 = contract(net)
+    c = yaocircuit_from_qasm(filename)
+    initial_state = Dict(zip(1:nqubits(c), zeros(Int,nqubits(c))))
+    final_state = Dict(zip(1:nqubits(c), bitstring))
+    net = yao2einsum(c; initial_state, final_state, optimizer, slicer=TreeSASlicer(score=ScoreFunction(sc_target=sc_target)))
 
-# ## Case 2: add noise and compute <ψ|X₁X₂|ψ>, where |ψ> = c |0>
-function add_noise(c::AbstractBlock; depolarizing1, depolarizing2)
+    # save the network
+    YaoToEinsum.save_tensor_network(net; folder=output)
+    return c
+end
+
+function dump_noisy_network(input::String; observable, depolarizing, output_folder, sc_target::Int=Inf, optimizer=TreeSA(ntrials=1), overwrite=false)
+    filename = joinpath(@__DIR__, "data", "circuits", input)
+    output = joinpath(@__DIR__, "data", "networks_noisy", output_folder)
+    isfile(output) && !overwrite && return
+
+    c = yaocircuit_from_qasm(filename)
+    noisy_c = add_depolarizing_noise(c, depolarizing)
+    initial_state = Dict(zip(1:nqubits(noisy_c), zeros(Int,nqubits(noisy_c))))
+    net = yao2einsum(noisy_c; initial_state, observable, optimizer, slicer=TreeSASlicer(score=ScoreFunction(sc_target=sc_target)), mode=DensityMatrixMode())
+
+    # save the network
+    YaoToEinsum.save_tensor_network(net; folder=output)
+    return noisy_c
+end
+
+function add_depolarizing_noise(c::AbstractBlock, depolarizing)
     Optimise.replace_block(c) do blk
-        if blk isa PrimitiveBlock && nqubits(blk) == 1  # two qubits
-            return chain(blk, DepolarizingChannel(1, depolarizing1))
-        elseif (blk isa PrimitiveBlock || blk isa ControlBlock) && nqubits(blk) == 2
-            return chain(blk, DepolarizingChannel(2, depolarizing2))
+        if blk isa PutBlock || blk isa ControlBlock
+            rep = chain(blk)
+            for loc in occupied_locs(blk)
+                push!(rep, put(nqubits(blk), loc=>DepolarizingChannel(1, depolarizing)))
+            end
+            return rep
         else
             return blk
         end
     end
 end
-noisy_c = add_noise(c; depolarizing1=0.01, depolarizing2=0.01)
-vizcircuit(noisy_c)
-time_elapsed = @elapsed net = yao2einsum(c; initial_state=Dict(zip(1:nqubits(c), zeros(Int,nqubits(c)))), observable=kron(nqubits(c), 1=>X, 2=>X), optimizer, mode=DensityMatrixMode())
-@info "Contraction complexity: $(contraction_complexity(net)), time cost: $(time_elapsed)s"
-# xx = contract(net)
+
+function main()
+    for filename in readdir(joinpath(@__DIR__, "data", "circuits"))
+        @info "Processing $filename"
+        c = dump_network(filename, bitstring=zeros(Int, nqubits(yaocircuit_from_qasm(joinpath(@__DIR__, "data", "circuits", filename)))), output_folder=filename[1:end-4], sc_target=30)
+        # net = load_tensor_network(joinpath(@__DIR__, "data", "networks", filename[1:end-4]))
+        dump_noisy_network(filename, observable=kron(nqubits(c), 1=>X, 2=>X), depolarizing=0.01, output_folder=filename[1:end-4], sc_target=30)
+    end
+end
+
+main()
